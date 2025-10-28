2dd4cd97b63848407805bd884583728cb505f7fc
JoaquinMurua <jmurua03@gmail.com>
2025-10-28 15:44:00 -0300

    Restaura la descripcion de una Localidad, habia sido borrada por equivocacion

--- src/localidad/localidad.entity.ts
+++ src/localidad/localidad.entity.ts
@@ -1,30 +1,44 @@
-import { Entity, Property, ManyToOne, Rel, Cascade, OneToMany, Collection } from "@mikro-orm/core";
-import { Provincia } from "../provincia/provincia.entity.js";
-import { BaseEntity } from "../shared/db/baseEntity.entity.js";
-import { PuntoDeInteres } from "../puntoDeInteres/puntoDeInteres.entity.js";
-import { Usuario } from "../usuario/usuario.entity.js";
-
+import {
+  Entity,
+  Property,
+  ManyToOne,
+  Rel,
+  Cascade,
+  OneToMany,
+  Collection,
+} from '@mikro-orm/core';
+import { Provincia } from '../provincia/provincia.entity.js';
+import { BaseEntity } from '../shared/db/baseEntity.entity.js';
+import { PuntoDeInteres } from '../puntoDeInteres/puntoDeInteres.entity.js';
+import { Usuario } from '../usuario/usuario.entity.js';
 
 @Entity()
 export class Localidad extends BaseEntity {
-    @Property({ nullable: false, unique: true , type: 'string'})
-    nombre!: string
+  @Property({ nullable: false, unique: true, type: 'string' })
+  nombre!: string;
 
-    @Property({ nullable: true, type: 'float' })
-    latitud!: number
+  @Property({ nullable: true, type: 'float' })
+  latitud!: number;
 
-    @Property({ nullable: true, type: 'float' })
-    longitud!: number
+  @Property({ nullable: true, type: 'float' })
+  longitud!: number;
 
-    @Property({ nullable: true, type: 'string' })
-    imagen?: string;
+  @Property({ nullable: true, type: 'string' })
+  imagen?: string;
 
-    @ManyToOne(() => Provincia, { nullable: false })
-    provincia!: Rel<Provincia>;
+  @Property({ nullable: true, unique: false, type: 'string' })
+  descripcion!: string;
 
-    @OneToMany(() => PuntoDeInteres, (PuntoDeInteres) => PuntoDeInteres.localidad, { nullable: true, cascade: [Cascade.ALL] })
-    puntosDeInteres = new Collection<PuntoDeInteres>(this);
+  @ManyToOne(() => Provincia, { nullable: false })
+  provincia!: Rel<Provincia>;
 
-    @OneToMany(()=> Usuario, (usuario) => usuario.localidad, {nullable: true }) //cascade: [Cascade.CANCEL_ORPHAN_REMOVAL] 
-    usuarios = new Collection <Usuario> (this)
+  @OneToMany(
+    () => PuntoDeInteres,
+    (PuntoDeInteres) => PuntoDeInteres.localidad,
+    { nullable: true, cascade: [Cascade.ALL] }
+  )
+  puntosDeInteres = new Collection<PuntoDeInteres>(this);
+
+  @OneToMany(() => Usuario, (usuario) => usuario.localidad, { nullable: true }) //cascade: [Cascade.CANCEL_ORPHAN_REMOVAL]
+  usuarios = new Collection<Usuario>(this);
 }
