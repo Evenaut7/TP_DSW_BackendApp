660ca0de416c2acf24b9544c1522c97926f4f2b1
Evenaut7 <valentinolaveggi@outlook.com>
2026-02-18 10:35:14 -0300

    Correccion de zod en localidad

--- src/localidad/localidad.schema.ts
+++ src/localidad/localidad.schema.ts
@@ -6,7 +6,7 @@ export const localidadSchema = z.object({
     descripcion: z.string().min(1, {message: "descripcion no puede estar vacía"}).max(1024, {message: "descripcion no puede superar los 1024 caracteres"}),
     fechaDesde: z.string().nonempty({message: "La fecha desde no puede estar vacia"}).refine((date) => !isNaN(Date.parse(date)), { message: "Formato de fecha Invalido" }),
     fechaHasta: z.string().nonempty({message: "La fecha hasta no puede estar vacia"}).refine((date) => !isNaN(Date.parse(date)), { message: "Formato de fecha Invalido" }),
-    puntoDeInteres: z.number({message: "El punto de interes es obligatorio"}).positive({message: "El id del punto de interes debe ser un numero positivo"}), 
+    puntoDeInteres: z.array(z.number().int().positive().refine(() => true, { message: "El punto de interes debe ser un número positivo" })),
     imagen: z.string().min(1, {message: "imagen no puede estar vacío"}).max(255, {message: "imagen no puede superar los 255 caracteres"}),
   })
 });
